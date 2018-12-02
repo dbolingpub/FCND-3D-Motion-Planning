80004aefe03a62d9daafd2aad231b787e8241f42
Daniel Boling <dbolingpub@github.com>
2018-12-02 17:06:31 +0000

    Path Planning v1 completed.

--- motion_planning.py
+++ motion_planning.py
@@ -5,7 +5,14 @@ from enum import Enum, auto
 
 import numpy as np
 
-from planning_utils_diag import a_star, heuristic, create_grid
+from planning_utils import (
+    a_star, 
+    create_grid, 
+    get_gridrelative_position, 
+    get_latlon_fromfile,
+    heuristic, 
+    prune_path
+)
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
@@ -24,7 +31,7 @@ class States(Enum):
 
 class MotionPlanning(Drone):
 
-    def __init__(self, connection):
+    def __init__(self, connection, goal_longitude, goal_latitude):
         super().__init__(connection)
 
         self.target_position = np.array([0.0, 0.0, 0.0])
@@ -39,6 +46,12 @@ class MotionPlanning(Drone):
         self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
         self.register_callback(MsgID.LOCAL_VELOCITY, self.velocity_callback)
         self.register_callback(MsgID.STATE, self.state_callback)
+
+        #goal coordinates
+        self.goal_longitude = goal_longitude
+        self.goal_latitude = goal_latitude
+
+        print("Longitude/Latitude (from args): {}, {}".format(self.goal_longitude, self.goal_latitude))
 
     def local_position_callback(self):
         if self.flight_state == States.TAKEOFF:
@@ -130,60 +143,24 @@ class MotionPlanning(Drone):
         debug = True
         test_run = False
 
-        # save orig values
-        orig_global_pos = self.global_position
-        orig_global_home = self.global_home
-        orig_local_pos = global_to_local(self.global_position, self.global_home)
-        
-        # 3.1: 
+        # Requirement #1: 
         # * Read lat0, lon0 from colliders into floating point values
         # * set home position to (lon0, lat0, 0)
-        lat0, lon0 = self.get_latlon_fromfile('colliders.csv')
+        lat0, lon0 = get_latlon_fromfile('colliders.csv')
         self.set_home_position(lon0, lat0, 0)
         
-        # 3.2: 
+        # Requirement #2: 
         # * retrieve current global position
-        # * convert to current local position using global_to_local()
-        
-        # orig: working
-        # curr_global_pos0 = self.global_position
-        # curr_global_home0 = self.global_home
-        # curr_local_pos0 = global_to_local(curr_global_pos0, curr_global_home0)
-        
-        # new 11/24/18 4:35 PM: 
+        # * convert to current local position using global_to_local() 
         curr_global_pos = (self._longitude, self._latitude, self._altitude)
         curr_global_home = self.global_home
         curr_local_pos = global_to_local(curr_global_pos, self.global_home)
         
         if debug:
             print()
-            print("curr_global_pos0: ", curr_global_pos0)
             print("curr_global_pos: ", curr_global_pos)
-
-            print()
-            print("curr_global_home0: ", curr_global_home0)
             print("curr_global_home: ", curr_global_home)
-
-            print()
-            print("curr_local_pos0: ", curr_local_pos0)
             print("curr_local_pos: ", curr_local_pos)
-        
-        
-        if debug:
-            print()
-            print("orig_global_home: ", orig_global_home)
-            print("new_global_home: ", curr_global_home)
-            print("are equal: ", orig_global_home==curr_global_home)
-            
-            print()
-            print("orig_global_pos: ", orig_global_pos)
-            print("curr_global_pos: ", curr_global_pos)
-            print("are equal: ", orig_global_pos==curr_global_pos)            
-            
-            print()
-            print("orig_local_pos: ", orig_local_pos)
-            print("curr_local_pos: ", curr_local_pos)
-            print("offset (n, e): ", curr_local_pos[0]-orig_local_pos[0], curr_local_pos[1]-orig_local_pos[1])
         
         #print('global home {0}\nposition {1}\nlocal position {2}'.format(self.global_home, self.global_position,
                                                                          #self.local_position))
@@ -195,36 +172,35 @@ class MotionPlanning(Drone):
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         
         # Define starting point on the grid (this is just grid center)
-        grid_start_test = (-north_offset, -east_offset)
-        print("grid_start (orig): ", grid_start_test)
+        # grid_start_test = (-north_offset, -east_offset)
+        # print("grid_start (orig): ", grid_start_test)
 
         # save offsets as tuple
         offsets = (north_offset, east_offset)
         print("offsets: ", offsets)
-
-        # use old for now
-        # curr_local_pos = orig_local_pos
         
-        # 3.3: convert start position to current position rather than map center
-        grid_start = self.get_gridrelative_position(curr_local_pos[0:2], offsets)
+        # Requirement #3: convert start position to current position rather than map center
+        grid_start = get_gridrelative_position(curr_local_pos[0:2], offsets)
         print("grid_start: ", grid_start)
 
         # Set goal as some arbitrary position on the grid
-        grid_goal_test = (-north_offset + 10, -east_offset + 10)
-        print("grid_goal (orig): ", grid_goal_test)
+        # grid_goal_test = (-north_offset + 10, -east_offset + 10)
+        # print("grid_goal (orig): ", grid_goal_test)
 
-        # 3.4: adapt to set goal as latitude / longitude position and convert
-        # set goal position as some desired position on the map
-        custom_goal_pos = (-122.398414, 37.7939265, 0)
+        # Requirement #4: set goal as latitude/longitude
+        # specified as command line args
+        custom_goal_pos = (self.goal_longitude, self.goal_latitude, 0)
         goal_local = global_to_local(custom_goal_pos, self.global_home)[0:2]
-        grid_goal = self.get_gridrelative_position(goal_local, offsets)
+        grid_goal = get_gridrelative_position(goal_local, offsets)
         print("grid_goal: ", grid_goal)
         
+        """
         print("is test run?: ", test_run)
         if test_run:
             # grid_start = grid_start_test
             grid_goal = grid_goal_test
             print("test run (start, end): ", (grid_start, grid_goal))
+        """
 
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
@@ -234,11 +210,8 @@ class MotionPlanning(Drone):
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         print('Path planning complete!')
         
-        # TODO: prune path to minimize number of waypoints
-        orig_path = path[:]
-        path = self.prune_path(path)
-        print("path length (pre-prune): ", len(orig_path))
-        print("path length (post-prune): ", len(path))
+        # Requirement 6: prune path to minimize number of waypoints
+        path = prune_path(path)
 
         # TODO (if you're feeling ambitious): Try a different approach altogether!
 
@@ -253,50 +226,8 @@ class MotionPlanning(Drone):
         # Set self.waypoints
         self.waypoints = waypoints
         
-        # TODO: send waypoints to sim (this is just for visualization of waypoints)
+        # Send waypoints to sim (this is just for visualization of waypoints)
         self.send_waypoints()
-
-    def prune_path(self, path):
-        """
-        Perform collinearily check on each set of 3 points and
-        determine those points that are collinear.
-        """
-        pruned_path = [p for p in path]
-        
-        i = 0
-        while i < len(pruned_path) - 2:
-            p1 = self.point(pruned_path[i])
-            p2 = self.point(pruned_path[i+1])
-            p3 = self.point(pruned_path[i+2])
-            
-            if self.collinearity_test(p1, p2, p3):
-                pruned_path.remove(pruned_path[i+1])
-            else:
-                i += 1
-                
-        return pruned_path
-
-    def point(self, p):
-        return np.array([p[0], p[1], 1.]).reshape(1, -1)
-
-    def collinearity_test(self, p1, p2, p3, epsilon=1e-6):   
-        m = np.concatenate((p1, p2, p3), 0)
-        det = np.linalg.det(m)
-        return abs(det) < epsilon
-
-    def get_gridrelative_position(self, local_position, offsets):
-        local_pos_north, local_pos_east = int(local_position[0]), int(local_position[1])
-        north_offset, east_offset = offsets
-        return (local_pos_north - north_offset, local_pos_east - east_offset)
-
-    def get_latlon_fromfile(self, input_file):
-        """Get latitude/longitude data from first line of input file."""
-        with open(input_file) as f:
-            parts = f.readline().replace(',', '').split()
-            lat = float(parts[1])
-            lon = float(parts[3])
-
-            return lat, lon
 
     def start(self):
         self.start_log("Logs", "NavLog.txt")
@@ -315,10 +246,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=5760, help='Port number')
     parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    # custom args
+    parser.add_argument('--goal_longitude', type=float, default=-122.398414, help='Goal Longitude')
+    parser.add_argument('--goal_latitude', type=float, default=37.7939265, help='Goal Latitude')
     args = parser.parse_args()
 
     conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), timeout=60)
-    drone = MotionPlanning(conn)
+    drone = MotionPlanning(conn, args.goal_longitude, args.goal_latitude)
     time.sleep(1)
 
     drone.start()
--- planning_utils.py
+++ planning_utils.py
@@ -53,7 +53,11 @@ class Action(Enum):
     WEST = (0, -1, 1)
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
-    SOUTH = (1, 0, 1)
+    SOUTH = (1, 0, 1)    
+    NORTH_WEST = (-1, -1, np.sqrt(2))
+    NORTH_EAST = (-1, 1, np.sqrt(2))
+    SOUTH_WEST = (1, -1, np.sqrt(2))
+    SOUTH_EAST = (1, 1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -82,6 +86,16 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
+
+    # Diagonal Actions
+    if x - 1 < 0 or y - 1 < 0 or grid[x - 1, y - 1] == 1:
+        valid_actions.remove(Action.NORTH_WEST)
+    if x - 1 < 0 or y + 1 > m or grid[x - 1, y + 1] == 1:
+        valid_actions.remove(Action.NORTH_EAST)
+    if x + 1 > n or y - 1 < 0 or grid[x + 1, y - 1] == 1:
+        valid_actions.remove(Action.SOUTH_WEST)  
+    if x + 1 > n or y + 1 > m or grid[x + 1, y + 1] == 1:
+        valid_actions.remove(Action.SOUTH_EAST)       
     
     return valid_actions
 
@@ -141,3 +155,45 @@ def a_star(grid, h, start, goal):
 def heuristic(position, goal_position):
     """Define a heuristic for scoring partial plans."""
     return np.linalg.norm(np.array(position) - np.array(goal_position))
+
+def prune_path(path):
+    """
+    Perform collinearily check on each set of 3 points and
+    determine those points that are collinear.
+    """
+    pruned_path = [p for p in path]
+    
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i+1])
+        p3 = point(pruned_path[i+2])
+        
+        if collinearity_test(p1, p2, p3):
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+            
+    return pruned_path
+
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+def collinearity_test(p1, p2, p3, epsilon=1e-6):   
+    m = np.concatenate((p1, p2, p3), 0)
+    det = np.linalg.det(m)
+    return abs(det) < epsilon
+
+def get_gridrelative_position(local_position, offsets):
+    local_pos_north, local_pos_east = int(local_position[0]), int(local_position[1])
+    north_offset, east_offset = offsets
+    return (local_pos_north - north_offset, local_pos_east - east_offset)
+
+def get_latlon_fromfile(input_file):
+    """Get latitude/longitude data from first line of input file."""
+    with open(input_file) as f:
+        parts = f.readline().replace(',', '').split()
+        lat = float(parts[1])
+        lon = float(parts[3])
+
+        return lat, lon
